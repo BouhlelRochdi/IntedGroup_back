448898ff18429508c48ec74604552103ff5071ff
Bouhlel Rochdi <rochdi.bouhlel@hotmail.fr>
2023-07-26 17:32:03 +0100

    finale version

--- routes/demand.routes.js
+++ routes/demand.routes.js
@@ -7,6 +7,7 @@ const { checkToken } = require("../middlewares/checkToken.middleware.js");
 router.post("/", checkToken, demand.createDemand);
 router.get("/", checkToken, demand.getDemands);
 router.get("/demande/:id", checkToken, demand.getDemandById);
+router.get("/demande-user/", checkToken, demand.demandesByUser);
 router.delete("/demande/:id", checkToken, demand.deleteDemandById);
 router.put("/demande/:id", checkToken, demand.demandRespond);
 
